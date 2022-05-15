84853fe64a7dd43fa3275ae9f463ae1a602f8fa7
Gabriel Juchault <gabriel.juchault@gmail.com>
2022-05-15 15:19:35 +0200

    chore(tests): application, repository tests

---
+++ src/application/healthcheck/__tests__/getHealthcheck.test.ts
@@ -0,0 +1,80 @@
+import test from "ava";
+import type { Redis } from "ioredis";
+import type { HealthcheckRepository } from "../../../repository/healthcheck";
+import { createGetHealthcheck } from "../getHealthcheck";
+
+const mockHealthyCache = {
+  echo: () => Promise.resolve("1"),
+} as unknown as Redis;
+
+const mockUnhealthyCache = {
+  echo: () => Promise.reject(),
+} as unknown as Redis;
+
+const mockHealthyRepository: HealthcheckRepository = {
+  async getHealthcheck() {
+    return { outcome: "healthy" };
+  },
+};
+
+const mockUnhealthyRepository: HealthcheckRepository = {
+  async getHealthcheck() {
+    return { outcome: "unhealthy" };
+  },
+};
+
+test("getHealthcheck() - healthy cache, database", async (t) => {
+  const getHealthcheck = createGetHealthcheck({
+    cache: mockHealthyCache,
+    healthcheckRepository: mockHealthyRepository,
+  });
+
+  const result = await getHealthcheck();
+
+  t.is(result.cache, "healthy");
+  t.is(result.database, "healthy");
+  t.is(result.systemMemory, "healthy");
+  t.is(result.processMemory, "healthy");
+});
+
+test("getHealthcheck() - unhealthy cache, healthy database", async (t) => {
+  const getHealthcheck = createGetHealthcheck({
+    cache: mockUnhealthyCache,
+    healthcheckRepository: mockHealthyRepository,
+  });
+
+  const result = await getHealthcheck();
+
+  t.is(result.cache, "unhealthy");
+  t.is(result.database, "healthy");
+  t.is(result.systemMemory, "healthy");
+  t.is(result.processMemory, "healthy");
+});
+
+test("getHealthcheck() - healthy cache, unhealthy database", async (t) => {
+  const getHealthcheck = createGetHealthcheck({
+    cache: mockHealthyCache,
+    healthcheckRepository: mockUnhealthyRepository,
+  });
+
+  const result = await getHealthcheck();
+
+  t.is(result.cache, "healthy");
+  t.is(result.database, "unhealthy");
+  t.is(result.systemMemory, "healthy");
+  t.is(result.processMemory, "healthy");
+});
+
+test("getHealthcheck() - unhealthy cache, unhealthy database", async (t) => {
+  const getHealthcheck = createGetHealthcheck({
+    cache: mockUnhealthyCache,
+    healthcheckRepository: mockUnhealthyRepository,
+  });
+
+  const result = await getHealthcheck();
+
+  t.is(result.cache, "unhealthy");
+  t.is(result.database, "unhealthy");
+  t.is(result.systemMemory, "healthy");
+  t.is(result.processMemory, "healthy");
+});
---
+++ src/repository/healthcheck/__tests__/getHealthcheck.test.ts
@@ -0,0 +1,36 @@
+import test from "ava";
+import sinon from "sinon";
+import { createMockPool, createMockQueryResult } from "slonik";
+import { createHealthcheckRepository } from "../";
+
+test("getHealthcheck() - healthy database", async (t) => {
+  const mockQuery = sinon.stub().resolves(createMockQueryResult([]));
+
+  const repository = createHealthcheckRepository({
+    database: createMockPool({
+      query: mockQuery,
+    }),
+  });
+
+  const result = await repository.getHealthcheck();
+
+  t.is(result.outcome, "healthy");
+  t.is(mockQuery.callCount, 1);
+  t.true(mockQuery.calledWith("select 1"));
+});
+
+test("getHealthcheck() - unhealthy database", async (t) => {
+  const mockQuery = sinon.stub().rejects(new Error("error"));
+
+  const repository = createHealthcheckRepository({
+    database: createMockPool({
+      query: mockQuery,
+    }),
+  });
+
+  const result = await repository.getHealthcheck();
+
+  t.is(result.outcome, "unhealthy");
+  t.is(mockQuery.callCount, 1);
+  t.true(mockQuery.calledWith("select 1"));
+});
