d4bd9a91c541a945a524485343e77f8e7928fadf
Gabriel Juchault <gabriel.juchault@gmail.com>
2022-05-17 13:59:57 +0200

    chore(package): dev & debug command

---
+++ scripts/dev.ts
@@ -0,0 +1,25 @@
+import path from "node:path";
+import { build as dev } from "estrella";
+
+process.env.NODE_ENV = "development";
+
+dev({
+  entry: path.join(__dirname, "../src/index.ts"),
+  outfile: path.join(__dirname, "../build/index.js"),
+  bundle: true,
+  run: [
+    process.execPath,
+    "--inspect=0.0.0.0:9229",
+    path.resolve(__dirname, "../build/index.js"),
+  ],
+  watch: true,
+  clear: false,
+  minify: false,
+
+  platform: "node",
+  target: "esnext",
+  nodePaths: [path.join(__dirname, "../src")],
+  sourcemap: true,
+  external: ["pg-native"],
+  format: "cjs",
+});
