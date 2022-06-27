1a2df35583e7281bb3f62084d3726ef01c3f9fbe
Gabriel Juchault <gabriel.juchault@gmail.com>
2022-06-27 15:16:55 +0200

    chore(tests): drop database and run migrations before tests, added example env file

--- src/test-helpers/integration-start-context.ts
+++ src/test-helpers/integration-start-context.ts
@@ -1,4 +1,6 @@
+import { sql } from "slonik";
 import { beforeAll } from "vitest";
+import { databasePool, migrate } from "../../scripts/migrate";
 import { startApp } from "../index";
 import type { HttpServer } from "../infrastructure/http";
 
@@ -9,6 +11,20 @@ beforeAll(async () => {
     port: 1987,
     logLevel: "error",
   });
+
+  await databasePool.query(
+    sql`
+      do $$ declare
+          r record;
+      begin
+          for r in (select tablename from pg_tables where schemaname not in ('pg_catalog', 'information_schema')) loop
+              execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+          end loop;
+      end $$;
+    `
+  );
+
+  await migrate(["up"], false);
 
   http = app.httpServer;
 
